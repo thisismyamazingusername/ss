cba944625c1bd068c847228b54a75f6d0f248f6c
thisismyamazingusername <111548413+thisismyamazingusername@users.noreply.github.com>
2024-06-09 14:35:59 -0500

    fixing use client in map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,40 +1,38 @@
-"use client"
+"use client";
 
 import React from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
-
-// Set default icon URLs
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import marketIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// Set default icon URLs
-L.Marker.prototype.options.icon = L.icon({
-  iconUrl: marketIcon.src as string,
-  iconRetinaUrl: markerIcon2x.src as string,
-  shadowUrl: markerShadow.src as string,
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: marketIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
 });
 
-
 interface MapProps {
-  center?: L.LatLngExpression;
+  center?: number[];
 }
 
 const Map: React.FC<MapProps> = ({ center }) => {
   return (
     <MapContainer
-      center={center || [52, -0.09]}
+      center={(center as L.LatLngExpression) || [52, -0.09]}
       zoom={center ? 4 : 2}
       scrollWheelZoom={false}
-      className={`h-full rounded-lg`}
+      className="h-full rounded-lg"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {center && <Marker position={center} />}
+      {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
 };
